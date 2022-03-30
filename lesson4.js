a2c47da15956475c60cb0407cf90d7a9996d3547
Lena <helenrv92@mail.ru>
2022-03-30 15:43:47 +0300

    lesson4

---
+++ lesson4.js
@@ -0,0 +1,25 @@
+let a = 'лена'
+let b = true
+//let g = '!'
+
+console.log (a / b )
+
+let c = 'лена'
+let d = 23
+//let g = '!'
+
+console.log (c / d )
+
+let f = 23
+let r = false
+//let g = '!'
+
+console.log (f / r )
+
+
+let value = '123';
+console.log(typeof value); 
+
+value = Number(value); 
+console.log(typeof value);
+
