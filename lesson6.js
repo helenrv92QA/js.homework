7e182368f31a93567d9d4528b1f41c86803f4a01
Lena <helenrv92@mail.ru>
2022-04-13 13:41:10 +0300

    lesson6

---
+++ lesson6.js
@@ -0,0 +1,78 @@
+// let arr = [1,2,3,4,5,7];
+// for (i=0;i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// for (i=1; i<=50; i++){
+//     console.log(i)
+// }
+
+// let arr = [2,3,4,5]
+
+// let sum = arr.reduce(function(a,b){
+//     return a*b
+ 
+// })
+// console.log(sum)
+
+
+// let str4 = '2025-12-31'
+
+// let newstr= str4.split('-').reverse().join("/")
+
+// console.log(newstr)
+
+
+// let str5 = 'hello World'
+
+// let newstr = str5[0].toUpperCase() + str5.slice(1)
+
+// console.log (newstr)
+
+
+// let str5 = 'hello world js'
+// let newstr = str5.split(" ");
+// let newstr1 = [];
+// for (i of newstr){
+//     newstr1.push(i[0].toUpperCase() + i.slice(1))
+// }
+// console.log (newstr1.join(' '))
+
+
+// let str5 = 'hello_world_js'
+// let newstr = str5.split("_");
+// let newstr1 = [];
+// for (i in newstr){
+//     newstr1.push(i[0].toUpperCase() + i.slice(1))
+// }
+// console.log (newstr1.join(''))
+
+
+// let obj = {name: "Lena", cat:"Tiger"};
+// console.log(Object.keys(obj));
+
+// let arr1 = [1,2,3]
+// let arr2 = [4,5,6]
+// let arr3 = [].concat(arr1,arr2)
+// console.log(arr3)
+
+// let arr1 = [1,2,3]
+// arr2 = arr1.reverse()
+// console.log(arr2)
+
+// let arr1 = [1,2,3]
+// let arr2 = arr1.push(4,5)+arr1.unshift(0)
+// console.log (arr1)
+
+// let arr1 = [1,2,3,4,5]
+// let arr2 = arr1.slice(0,3)
+// console.log(arr2)
+
+// let arr1 = [1,2,3,4,5,21,14,6]
+// let arr3 = arr1.sort((a,b) => a-b)
+// console.log(arr3)
+let a = 6
+if(a<5){
+    console.log('Верно')
+} else {console.log('неВерно')}
